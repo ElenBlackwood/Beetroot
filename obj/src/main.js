025c5173d57b9fe1710e2f8e917a253473089470
ElenBlackwood <lena.pr.1998@gmail.com>
2022-04-19 17:12:27 +0300

    add objects js

---
+++ obj/src/main.js
@@ -0,0 +1,32 @@
+let volvo = {
+    dealer: 'Poland',
+    model: 'XC40 T5 AWD',
+    yearOfManufacture: 2022,
+    averageFuelConsumption: '7,2 л / 100 км',
+    // volume of a fuel tank: 
+    nameOfDrivers: 'Elena',
+
+    // function calcTime(s, v, t) {
+    //     s = 1920, // відстань в км
+    //     v = 120, // км/год
+    //     s4 == v * 4, // скільки відстані потрібно на 4 години
+    //     t4 == s / s4, // скільки год на відпочинок треба
+    //     t == (s / v) + t4, // формула 
+    // }
+
+    
+}
+
+console.log(volvo);
+console.log(calcTime(t));
+
+//
+
+let descrTime = {
+    sec: 55,
+    min: 35,
+    hours: 3,
+}
+
+console.log(descrTime);
+
