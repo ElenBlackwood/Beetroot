0cbd732a980edc7e40586ef692d74496a5d4d2c8
ElenBlackwood <97550447+ElenBlackwood@users.noreply.github.com>
2022-05-25 17:11:09 +0300

    Delete mac directory

--- mac/src/style/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-    $('.hamburger').on('click', function() {
-        if ($('.header').hasClass('active')) {
-            $('.header').removeClass('active');
-        } else {
-            $('.header').addClass('active');
-        }
-    })
-})
